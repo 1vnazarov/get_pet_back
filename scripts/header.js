1dc82dce7780a081392ea97a1697767ac11754a6
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-11-30 17:26:34 +0300

    Правки ссылок

--- scripts/header.js
+++ scripts/header.js
@@ -3,7 +3,7 @@ window.onload = function() {
     header.innerHTML = `
 <nav class="navbar navbar-expand-sm bg-primary" data-bs-theme="dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="./index.html"><img src="../images/logo.png" alt="logo" class="w-25 rounded-3"></a>
+                <a class="navbar-brand" href="index.html"><img src="../images/logo.png" alt="logo" class="w-25 rounded-3"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -13,23 +13,23 @@ window.onload = function() {
                 <div class="collapse navbar-collapse" id="navbarsExample03">
                     <ul class="navbar-nav me-auto mb-2 mb-sm-0">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="./index.html" id="gotoMain">Главная</a>
+                            <a class="nav-link" aria-current="page" href="../index.html" id="gotoMain">Главная</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
                                 aria-expanded="false">Личный кабинет</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="./profile.html">Личный кабинет</a></li>
-                                <li><a class="dropdown-item" href="./auth.html">Войти</a></li>
-                                <li><a class="dropdown-item" href="./register.html">Регистрация</a></li>
+                                <li><a class="dropdown-item" href="../html/profile.html">Личный кабинет</a></li>
+                                <li><a class="dropdown-item" href="../html/auth.html">Войти</a></li>
+                                <li><a class="dropdown-item" href="../html/register.html">Регистрация</a></li>
                             </ul>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
                                 aria-expanded="false">Объявления</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="./add.html">Добавить объявление</a></li>
-                                <li><a class="dropdown-item" href="./search.html">Поиск по объявлению</a></li>
+                                <li><a class="dropdown-item" href="../html/add.html">Добавить объявление</a></li>
+                                <li><a class="dropdown-item" href="../html/search.html">Поиск по объявлению</a></li>
                             </ul>
                         </li>
                     </ul>
